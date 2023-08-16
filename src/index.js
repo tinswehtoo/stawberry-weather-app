83417d6b48a570a18d1f80b56c425be205fbb539
tinswehtoo <141896669+tinswehtoo@users.noreply.github.com>
2023-08-16 22:12:45 +0630

    add the weather forecast in js

--- src/index.js
+++ src/index.js
@@ -21,6 +21,31 @@ function formatDate(timestamp) {
   return `${day}${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+              <div class="weather-forecast-date">${day}</div>
+              <img
+                src="https://openweathermap.org/img/wn/02d@2x.png"
+                alt=""
+                width="42"
+              />
+              <div class="weather-forecsat-temperatures">
+                <span class="weather-forecast-temperature-max">18° </span>
+                <span class="weather-forecast-temperature-min">12°</span>
+              </div>
+            </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -87,3 +112,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+displayForecast();
